ff79baa4fea4a7f2ea06aba676a037885ca54683
Yousef Shaban <yousefshaban.eg@gmail.com>
2021-05-07 21:28:36 +0200

    Create Attendence API Controller.

--- GigHub/Controllers/Api/AttendancesController.cs
+++ GigHub/Controllers/Api/AttendancesController.cs
@@ -1,13 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Linq;
 using System.Web.Http;
+using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
 
 namespace GigHub.Controllers.Api
 {
+    [Authorize]
     public class AttendancesController : ApiController
     {
+        private readonly ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+            
+            if (_context.Attendances.Any(a => a.GigId == dto.GigId && a.AttendeeId == userId ))
+                return BadRequest("The Attendance is already exist.");
+            
+            var attendance = new Attendance
+            {
+                GigId = dto.GigId,
+                AttendeeId = userId
+            };
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+            return Ok();
+        }
     }
 }
